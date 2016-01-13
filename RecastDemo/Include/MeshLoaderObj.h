0468c2815922d7833b6c14d42f281f0c92333959
Graham Pentheny <graham.pentheny@gmail.com>
2016-01-13 17:45:21 -0500

    obj model loading uses STL file IO

--- RecastDemo/Include/MeshLoaderObj.h
+++ RecastDemo/Include/MeshLoaderObj.h
@@ -20,34 +20,28 @@
 #define MESHLOADER_OBJ
 
 #include <string>
+#include <vector>
 
 class rcMeshLoaderObj
 {
 public:
 	rcMeshLoaderObj();
-	~rcMeshLoaderObj();
 	
 	bool load(const std::string& fileName);
 
-	const float* getVerts() const { return m_verts; }
-	const float* getNormals() const { return m_normals; }
-	const int* getTris() const { return m_tris; }
-	int getVertCount() const { return m_vertCount; }
-	int getTriCount() const { return m_triCount; }
+	const float* getVerts() const { return m_verts.data(); }
+	const float* getNormals() const { return m_normals.data(); }
+	const int* getTris() const { return m_tris.data(); }
+	int getVertCount() const { return m_verts.size() / 3; }
+	int getTriCount() const { return m_tris.size() / 3; }
 	const std::string& getFileName() const { return m_filename; }
 
 private:
-	
-	void addVertex(float x, float y, float z, int& cap);
-	void addTriangle(int a, int b, int c, int& cap);
-	
 	std::string m_filename;
-	float m_scale;	
-	float* m_verts;
-	int* m_tris;
-	float* m_normals;
-	int m_vertCount;
-	int m_triCount;
+	std::vector<float> m_verts;
+	std::vector<int> m_tris;
+	std::vector<float> m_normals;
+	float m_scale;
 };
 
 #endif // MESHLOADER_OBJ
--- RecastDemo/Source/MeshLoaderObj.cpp
+++ RecastDemo/Source/MeshLoaderObj.cpp
@@ -17,213 +17,105 @@
 //
 
 #include "MeshLoaderObj.h"
+
+#include "Recast.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using std::string;
+using std::vector;
+
 rcMeshLoaderObj::rcMeshLoaderObj() :
-	m_scale(1.0f),
-	m_verts(0),
-	m_tris(0),
-	m_normals(0),
-	m_vertCount(0),
-	m_triCount(0)
+	m_scale(1.0f)
 {
-}
-
-rcMeshLoaderObj::~rcMeshLoaderObj()
-{
-	delete [] m_verts;
-	delete [] m_normals;
-	delete [] m_tris;
-}
-		
-void rcMeshLoaderObj::addVertex(float x, float y, float z, int& cap)
-{
-	if (m_vertCount+1 > cap)
-	{
-		cap = !cap ? 8 : cap*2;
-		float* nv = new float[cap*3];
-		if (m_vertCount)
-			memcpy(nv, m_verts, m_vertCount*3*sizeof(float));
-		delete [] m_verts;
-		m_verts = nv;
-	}
-	float* dst = &m_verts[m_vertCount*3];
-	*dst++ = x*m_scale;
-	*dst++ = y*m_scale;
-	*dst++ = z*m_scale;
-	m_vertCount++;
-}
-
-void rcMeshLoaderObj::addTriangle(int a, int b, int c, int& cap)
-{
-	if (m_triCount+1 > cap)
-	{
-		cap = !cap ? 8 : cap*2;
-		int* nv = new int[cap*3];
-		if (m_triCount)
-			memcpy(nv, m_tris, m_triCount*3*sizeof(int));
-		delete [] m_tris;
-		m_tris = nv;
-	}
-	int* dst = &m_tris[m_triCount*3];
-	*dst++ = a;
-	*dst++ = b;
-	*dst++ = c;
-	m_triCount++;
-}
-
-static char* parseRow(char* buf, char* bufEnd, char* row, int len)
-{
-	bool start = true;
-	bool done = false;
-	int n = 0;
-	while (!done && buf < bufEnd)
-	{
-		char c = *buf;
-		buf++;
-		// multirow
-		switch (c)
-		{
-			case '\\':
-				break;
-			case '\n':
-				if (start) break;
-				done = true;
-				break;
-			case '\r':
-				break;
-			case '\t':
-			case ' ':
-				if (start) break;
-			default:
-				start = false;
-				row[n++] = c;
-				if (n >= len-1)
-					done = true;
-				break;
-		}
-	}
-	row[n] = '\0';
-	return buf;
-}
-
-static int parseFace(char* row, int* data, int n, int vcnt)
-{
-	int j = 0;
-	while (*row != '\0')
-	{
-		// Skip initial white space
-		while (*row != '\0' && (*row == ' ' || *row == '\t'))
-			row++;
-		char* s = row;
-		// Find vertex delimiter and terminated the string there for conversion.
-		while (*row != '\0' && *row != ' ' && *row != '\t')
-		{
-			if (*row == '/') *row = '\0';
-			row++;
-		}
-		if (*s == '\0')
-			continue;
-		int vi = atoi(s);
-		data[j++] = vi < 0 ? vi+vcnt : vi-1;
-		if (j >= n) return j;
-	}
-	return j;
 }
 
 bool rcMeshLoaderObj::load(const std::string& filename)
 {
-	char* buf = 0;
-	FILE* fp = fopen(filename.c_str(), "rb");
-	if (!fp)
-		return false;
-	fseek(fp, 0, SEEK_END);
-	int bufSize = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	buf = new char[bufSize];
-	if (!buf)
-	{
-		fclose(fp);
-		return false;
-	}
-	size_t readLen = fread(buf, bufSize, 1, fp);
-	fclose(fp);
+	std::ifstream meshFile;
+	meshFile.open(filename);
 
-	if (readLen != 1)
-	{
-		delete[] buf;
-		return false;
-	}
+ 	string type;
+	string skip;
 
-	char* src = buf;
-	char* srcEnd = buf + bufSize;
-	char row[512];
-	int face[32];
-	float x,y,z;
-	int nv;
-	int vcap = 0;
-	int tcap = 0;
-	
-	while (src < srcEnd)
+	// This helps mitigate lots of vector resizes.
+	// We don't actually know how many verts or tris
+	// there are, but 10k is a reasonable guess.
+	m_verts.reserve(10000);
+	m_tris.reserve(10000);
+
+	string v1, v2, v3;
+
+	// Read mesh
+	while (!meshFile.eof())
 	{
-		// Parse one row
-		row[0] = '\0';
-		src = parseRow(src, srcEnd, row, sizeof(row)/sizeof(char));
-		// Skip comments
-		if (row[0] == '#') continue;
-		if (row[0] == 'v' && row[1] != 'n' && row[1] != 't')
+		// Get the line type.
+		meshFile >> type;
+
+		if (type == "v")
 		{
-			// Vertex pos
-			sscanf(row+1, "%f %f %f", &x, &y, &z);
-			addVertex(x, y, z, vcap);
+			// Vertex position
+			float x, y, z;
+			meshFile >> x >> y >> z;
+			m_verts.push_back(x * m_scale);
+			m_verts.push_back(y * m_scale);
+			m_verts.push_back(z * m_scale);
 		}
-		if (row[0] == 'f')
+		else if (type == "f")
 		{
-			// Faces
-			nv = parseFace(row+1, face, 32, m_vertCount);
-			for (int i = 2; i < nv; ++i)
+			// Face
+			meshFile >> v1 >> v2 >> v3;
+
+			// Only grab the vertex position index, not the normal or texture coordinate indices.
+			v1 = v1.substr(0, v1.find('/'));
+			v2 = v2.substr(0, v2.find('/'));
+			v3 = v3.substr(0, v3.find('/'));
+
+			int a = atoi(v1.c_str());
+			int b = atoi(v2.c_str());
+			int c = atoi(v3.c_str());
+
+			// Only add the face if the vert indices are valid.
+			if (a > 0 && a <= m_verts.size() && b > 0 && b <= m_verts.size() && c > 0 && c <= m_verts.size())
 			{
-				const int a = face[0];
-				const int b = face[i-1];
-				const int c = face[i];
-				if (a < 0 || a >= m_vertCount || b < 0 || b >= m_vertCount || c < 0 || c >= m_vertCount)
-					continue;
-				addTriangle(a, b, c, tcap);
+				m_tris.push_back(a - 1);
+				m_tris.push_back(b - 1);
+				m_tris.push_back(c - 1);
 			}
 		}
+		else
+		{
+			// Skip every other kind of line.  This line reads from the stream up to the next newline.
+			std::getline(meshFile, skip);
+		}
 	}
 
-	delete [] buf;
-
-	// Calculate normals.
-	m_normals = new float[m_triCount*3];
-	for (int i = 0; i < m_triCount*3; i += 3)
+	// Calculate face normals.
+	m_normals.resize(m_tris.size());
+	for (int i = 0; i < m_tris.size(); i += 3)
 	{
-		const float* v0 = &m_verts[m_tris[i]*3];
-		const float* v1 = &m_verts[m_tris[i+1]*3];
-		const float* v2 = &m_verts[m_tris[i+2]*3];
-		float e0[3], e1[3];
-		for (int j = 0; j < 3; ++j)
-		{
-			e0[j] = v1[j] - v0[j];
-			e1[j] = v2[j] - v0[j];
-		}
-		float* n = &m_normals[i];
-		n[0] = e0[1]*e1[2] - e0[2]*e1[1];
-		n[1] = e0[2]*e1[0] - e0[0]*e1[2];
-		n[2] = e0[0]*e1[1] - e0[1]*e1[0];
-		float d = sqrtf(n[0]*n[0] + n[1]*n[1] + n[2]*n[2]);
-		if (d > 0)
-		{
-			d = 1.0f/d;
-			n[0] *= d;
-			n[1] *= d;
-			n[2] *= d;
-		}
+		// Get pointers to the tri's vertex data.
+		const float* v0 = &m_verts[m_tris[i] * 3];
+		const float* v1 = &m_verts[m_tris[i + 1] * 3];
+		const float* v2 = &m_verts[m_tris[i + 2] * 3];
+		
+		// Calculate 2 edges of the triangle.
+		float edge0[3], edge1[3];
+		rcVsub(edge0, v1, v0);
+		rcVsub(edge1, v2, v0);
+
+		// Compute and normalize the dot product of the two edges.
+		float* normal = &m_normals[i];
+		rcVcross(normal, edge0, edge1);
+		rcVnormalize(normal);
 	}
 	
 	m_filename = filename;
